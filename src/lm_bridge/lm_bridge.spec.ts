ab0db6e2ee647e9e7c4133c94531c432154551b3
Eunchong Yu <gracie@wrtn.io>
2025-02-03 16:22:48 +0900

    test: add cases for exponential backoff

--- src/lm_bridge/lm_bridge.spec.ts
+++ src/lm_bridge/lm_bridge.spec.ts
@@ -1,8 +1,16 @@
-import { describe, expect, it, vi } from "vitest";
-import { LmBridge } from "./lm_bridge";
-import { Connection } from "./backend";
+import {
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+import { BackoffError, DEFAULT_BACKOFF_STRATEGY, LmBridge } from "./lm_bridge";
+import { Backend, Connection } from "./backend";
 import { Completion } from "./outputs/completion";
-import { ConsoleLogger } from "../logger";
+import { NoopLogger } from "../logger";
 
 const DUMMY_CONNECTION: Connection = {
   kind: { kind: "openai", model: "gpt-4o-2024-11-20" },
@@ -12,32 +20,38 @@ const DUMMY_CONNECTION: Connection = {
 const DUMMY_SESSION_ID = "279a6fa0-35ad-4605-9916-d8ca074b4354";
 
 describe("LmBridge.request", () => {
-  it("should return a completion", async () => {
-    const bridge = new LmBridge({ temperature: 0.2, logger: ConsoleLogger });
-    const mockBackendFactory = vi.fn(() => ({
-      kind: () => DUMMY_CONNECTION.kind,
-      baseUrl: "https://api.openai.com/v1",
-      makeCompletion: vi.fn(
-        async (): Promise<Completion> => ({
-          model: "gpt-4o-2024-11-20",
-          completionId: "bazqux",
-          messages: [
-            {
-              type: "text",
-              role: "assistant",
-              text: "Hello, world!",
-            },
-          ],
-          usage: {
-            inputTokens: 42,
-            outputTokens: 42,
-          },
-          isTruncated: false,
-          modelResponseMs: 0,
-        })
-      ),
-    }));
+  const mockMakeCompletion = vi.fn<() => Promise<Completion>>();
+  const mockBackendFactory = vi.fn<() => Backend>(() => ({
+    kind: () => DUMMY_CONNECTION.kind,
+    baseUrl: "https://api.openai.com/v1",
+    makeCompletion: mockMakeCompletion,
+  }));
+  const bridge = new LmBridge({ temperature: 0.2, logger: NoopLogger });
+
+  beforeAll(() => {
     bridge.backendFactory = mockBackendFactory;
+  });
+
+  beforeEach(() => {
+    mockMakeCompletion.mockResolvedValue({
+      model: "gpt-4o-2024-11-20",
+      completionId: "bazqux",
+      messages: [],
+      usage: {
+        inputTokens: 42,
+        outputTokens: 42,
+      },
+      isTruncated: false,
+      modelResponseMs: 0,
+    });
+  });
+
+  afterEach(() => {
+    mockMakeCompletion.mockReset();
+    mockBackendFactory.mockReset();
+  });
+
+  it("should return a completion", async () => {
     const completion = await bridge.request({
       connection: DUMMY_CONNECTION,
       sessionId: DUMMY_SESSION_ID,
@@ -65,23 +79,6 @@ describe("LmBridge.request", () => {
       logger: mockLogger,
       hasCostLog: true,
     });
-    const mockBackendFactory = vi.fn(() => ({
-      kind: () => DUMMY_CONNECTION.kind,
-      baseUrl: "https://api.openai.com/v1",
-      makeCompletion: vi.fn(
-        async (): Promise<Completion> => ({
-          model: "gpt-4o-2024-11-20",
-          completionId: "bazqux",
-          messages: [],
-          usage: {
-            inputTokens: 42,
-            outputTokens: 42,
-          },
-          isTruncated: false,
-          modelResponseMs: 0,
-        })
-      ),
-    }));
     bridge.backendFactory = mockBackendFactory;
     await bridge.request({
       connection: DUMMY_CONNECTION,
@@ -96,5 +93,36 @@ describe("LmBridge.request", () => {
     expect(json.output_tokens).toBe(42);
     expect(json.created_at).toBeDefined();
     expect(json.model_response_ms).toBeDefined();
+  });
+
+  it("should retry on too many requests", async () => {
+    mockMakeCompletion
+      .mockRejectedValueOnce(new BackoffError())
+      .mockRejectedValueOnce(new BackoffError());
+    const completion = await bridge.request({
+      connection: DUMMY_CONNECTION,
+      sessionId: DUMMY_SESSION_ID,
+      stageName: "test",
+      messages: [],
+    });
+    expect(completion).toBeDefined();
+    expect(mockBackendFactory).toHaveBeenCalledWith(DUMMY_CONNECTION);
+  });
+
+  it("should not retry if the backoff strategy describes no retries", async () => {
+    mockMakeCompletion.mockRejectedValueOnce(new BackoffError());
+    await expect(() =>
+      bridge.request({
+        connection: DUMMY_CONNECTION,
+        sessionId: DUMMY_SESSION_ID,
+        stageName: "test",
+        messages: [],
+        backoffStrategy: {
+          ...DEFAULT_BACKOFF_STRATEGY,
+          maxRetries: 1,
+        },
+      })
+    ).rejects.toThrowError();
+    expect(mockMakeCompletion).toHaveBeenCalledTimes(1);
   });
 });
